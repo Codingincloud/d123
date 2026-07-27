93725bf215d7012bb455bac47f45dbe9b9067b16
dristi-stha <sthadristi1@gmail.com>
2026-07-27 11:45:15 +0545

    Initial Django project setup

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+def home(request):
+    return render(request, "base.html")
+def index(request):
+    return render(request, "index.html")
+
+
+# Create your views here.
