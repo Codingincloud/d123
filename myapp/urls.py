fc13be2e6a2595699c0024ebfc832da90379878a
dristi-stha <sthadristi1@gmail.com>
2026-07-27 13:55:46 +0545

    Initial design

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("index/", views.index, name="index")
+    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("home/", views.home, name="home"),
+    path("contact/", views.contact, name="contact")
+
+
+
 
 ]
