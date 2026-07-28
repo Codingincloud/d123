c035590c5b565a025d5af953cf80c5e2deb52fd6
dristi-stha <sthadristi1@gmail.com>
2026-07-28 21:21:19 +0545

    postgres

--- nutriai/settings.py
+++ nutriai/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +80,11 @@ WSGI_APPLICATION = 'nutriai.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "nutriai",
-        "USER": "rominakoju",
-        "PASSWORD": "123",  # Add your password if you set one
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
