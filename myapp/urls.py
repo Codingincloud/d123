43973d2b3bc1d25e60473954d504866bad446e09
dristi-stha <sthadristi1@gmail.com>
2026-07-28 14:17:11 +0545

    Add responsive navbar and user dashboard

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,18 +1,42 @@
+# from django.urls import path
+# from . import views
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("index/", views.index, name="index"),
+#     path("about/", views.about, name="about"),
+#     path("home/", views.home, name="home"),
+#     path("contact/", views.contact, name="contact"),
+#     path("register/", views.register, name="register"),
+#     path("login/", views.user_login, name="login"),
+#     path("logout/", views.user_logout, name="logout"),
+#     path("userdash/",views.userdash, name="userdash")
+
+# ]
+
+
+
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
+
     path("", views.index, name="index"),
-    path("index/", views.index, name="index"),
-    path("about/", views.about, name="about"),
+
     path("home/", views.home, name="home"),
+
+    path("about/", views.about, name="about"),
+
     path("contact/", views.contact, name="contact"),
-    
-    
+
+
     path("register/", views.register, name="register"),
+
     path("login/", views.user_login, name="login"),
+
     path("logout/", views.user_logout, name="logout"),
 
 
-
+    path("dashboard/", views.userdash, name="userdash"),
 
 ]
